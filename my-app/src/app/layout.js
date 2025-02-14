726e6a691b4b4e2add9cbc16e92c93ea4ffc5254
이승민 <seungminlee@harami.co.kr>
2025-02-14 17:53:03 +0900

    20250214_2

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,11 +1,13 @@
 import { NavLinks } from './ui/nav-links'
 import { Imports } from './imports'
+import { PostList } from './PostList'
 export default function RootLayout({ children }) {
   console.log("children: " + children);
   return (
     <html lang="en">
       <body>
       <Imports />
+      <PostList />
         <div>
           <h2>next test !!!</h2>
           <h6>BIG TEXT11111</h6>
